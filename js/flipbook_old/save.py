b3f6ef36df1b943d2988e4eb0246d392d6ca9420
Jared Forsyth <jared@jaredforsyth.com>
2013-08-28 22:15:12 -0600

    cleanuo

--- js/flipbook_old/save.py
+++
@@ -1,8 +0,0 @@
-#! /usr/local/bin/python 
-# 
-from html2 import * 
-start("Online Editor")
-#! /usr/local/bin/python
-#
-from html2 import *
-start("err")
--- js/flipbook_old/tst.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$im = imageCreateFromPNG("img01.png") ;
-header('Content-type: image/gif');
-$e=imageGIF($im);
-//echo "FAILED?";
-//echo $e;
-imageDestroy($im);
-?>
